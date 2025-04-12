9b77755b828b4d45ee10dfca6e2770675f0f0278
1foxy2 <yarushas7@gmail.com>
2025-04-12 12:58:01 +0200

    Tooltip remover

---
+++ common/src/main/java/net/foxy/effectblacklister/mixin/ItemStackMixin.java
@@ -0,0 +1,36 @@
+package net.foxy.effectblacklister.mixin;
+
+import net.foxy.effectblacklister.EffectBlackLister;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.TranslatableContents;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+    @Inject(
+            method = "getTooltipLines",
+            at = @At(value = "RETURN")
+    )
+    private void removeEffectTooltip(Player player, TooltipFlag isAdvanced,
+                                            CallbackInfoReturnable<List<Component>> cir) {
+        if (EffectBlackLister.CONFIG.getKey().FOODS.get()
+                .contains(BuiltInRegistries.ITEM.getKey(((ItemStack) (Object) this).getItem()).toString())) {
+            cir.getReturnValue().removeIf(component -> {
+                if (component.getContents() instanceof TranslatableContents contents) {
+                    String key = contents.getKey();
+                    return key.equals("potion.withAmplifier") || key.equals("potion.withDuration");
+                }
+                return false;
+            });
+        }
+    }
+}
