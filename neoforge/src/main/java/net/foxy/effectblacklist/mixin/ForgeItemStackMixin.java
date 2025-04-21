43b2b8440232e279475648165433bb1110fcf2f5
1foxy2 <yarushas7@gmail.com>
2025-04-21 22:15:10 +0200

    Fix food black list

--- neoforge/src/main/java/net/foxy/effectblacklist/mixin/ForgeItemStackMixin.java
+++ neoforge/src/main/java/net/foxy/effectblacklist/mixin/ForgeItemStackMixin.java
@@ -1,5 +1,7 @@
 package net.foxy.effectblacklist.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.foxy.effectblacklist.EffectBlackList;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.LivingEntity;
@@ -9,13 +11,12 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.extensions.IForgeItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
 
-@Mixin(IForgeItemStack.class)
-public interface ForgeItemStackMixin {
-    @Redirect(
+@Mixin(ItemStack.class)
+public abstract class ForgeItemStackMixin implements IForgeItemStack {
+    @WrapOperation(
             method = "getFoodProperties",
             at = @At(
                     value = "INVOKE",
@@ -24,8 +25,9 @@ public interface ForgeItemStackMixin {
             ),
             remap = false
     )
-    default FoodProperties replaceFoodProperties(Item instance, ItemStack itemStack, LivingEntity livingEntity) {
-        FoodProperties properties = instance.getFoodProperties(itemStack, livingEntity);
+    FoodProperties replaceFoodProperties(Item instance, ItemStack itemStack, LivingEntity livingEntity,
+                                         Operation<FoodProperties> original) {
+        FoodProperties properties = original.call(instance, itemStack, livingEntity);
 
         if (properties == null || properties.getEffects().isEmpty()) {
             return properties;
