dd4a5179b5ab00650122a0d2def28ed0870de52e
1foxy2 <yarushas7@gmail.com>
2025-04-21 23:16:50 +0200

    Bump version

--- neoforge/src/main/java/net/foxy/effectblacklist/mixin/ForgeItemStackMixin.java
+++ neoforge/src/main/java/net/foxy/effectblacklist/mixin/ForgeItemStackMixin.java
@@ -1,7 +1,5 @@
 package net.foxy.effectblacklist.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.foxy.effectblacklist.EffectBlackList;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.LivingEntity;
@@ -10,30 +8,22 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.extensions.IForgeItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.List;
 
 @Mixin(ItemStack.class)
 public abstract class ForgeItemStackMixin implements IForgeItemStack {
-    @WrapOperation(
-            method = "getFoodProperties",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/item/Item;getFoodProperties(Lnet/minecraft/world/item/ItemStack;" +
-                            "Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/food/FoodProperties;"
-            ),
-            remap = false
-    )
-    FoodProperties replaceFoodProperties(Item instance, ItemStack itemStack, LivingEntity livingEntity,
-                                         Operation<FoodProperties> original) {
-        FoodProperties properties = original.call(instance, itemStack, livingEntity);
+    @Shadow public abstract Item getItem();
 
+    @Override
+    public FoodProperties getFoodProperties(LivingEntity entity) {
+        FoodProperties properties = getItem().getFoodProperties((ItemStack) (Object) this, entity);
         if (properties == null || properties.getEffects().isEmpty()) {
             return properties;
         }
 
-        if (EffectBlackList.CONFIG.getKey().FOODS.get().contains(BuiltInRegistries.ITEM.getKey(instance).toString())) {
+        if (EffectBlackList.CONFIG.getKey().FOODS.get().contains(BuiltInRegistries.ITEM.getKey(getItem()).toString())) {
             return new FoodProperties(properties.getNutrition(), properties.getSaturationModifier(),
                     properties.isMeat(), properties.canAlwaysEat(), properties.isFastFood(), List.of());
         }
